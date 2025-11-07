7188d1021ac3c16143c70fad95ba84c5fb53e186
esben_sorensen <ebsoren@umich.edu>
2025-11-07 13:58:34 -0500

    hello techcom

---
+++ techcom-boardgame-app/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-white font-sans text-2xl text-black">
+      hello TECHCOM!
+    </main>
+  );
+}
